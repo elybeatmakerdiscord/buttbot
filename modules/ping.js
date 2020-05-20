b10e175820218d510592c6ac2f1547af58663a98
Adryd <me@adryd.com>
2020-05-19 23:44:32 -0400

    added thing for JC :)

--- modules/ping.js
+++ modules/ping.js
@@ -1,7 +1,7 @@
 function ping(msg, args, ctx) {
     const pongMsgPool = ['Pong.',
         'PING discordapp.com (162.159.129.233): 56 data bytes\n64 bytes from 162.159.129.233: icmp_seq=0 ttl=56 time=17.979 ms',
-        'PoNg.', 'pong', 'Pong', ':ping_pong:', ':ping_pong: Pong', 'whoops, i missed', 'no', 'PONG', 'gnoP', 'Pong³', '³', 'p³ng', '𝒫𝒪𝒩𝒢', '𝐏𝐎𝐍𝐆', 'what does that mean', 'poggers', '<:YeetAary:664279299255435267>', 'now you\'re thinking with portals', 'stop poking me', `Pong!, ${msg.member.id / 2}ms`, `Pong!, ${msg.member.id / 4}ms`, `Pong!, ${msg.member.id / 32}ms`, `Pong!, -${msg.member.id / 1.5}ms`];
+        'PoNg.', 'pong', 'Pong', ':ping_pong:', ':ping_pong: Pong', 'whoops, i missed', 'no', 'PONG', 'gnoP', 'Pong³', '³', 'p³ng', '𝒫𝒪𝒩𝒢', '𝐏𝐎𝐍𝐆', 'what does that mean', 'poggers', '<:YeetAary:664279299255435267>', 'now you\'re thinking with portals', 'stop poking me', `Pong!, ${msg.member.id / 2}ms`, `Pong!, ${msg.member.id / 4}ms`, `Pong!, ${msg.member.id / 32}ms`, `Pong!, -${msg.member.id / 1.5}ms`, 'stop fucking pinging me omg'];
     const rng = (Math.random() * 10);
     if (rng < 1) {
         msg.channel.createMessage(pongMsgPool[Math.floor(Math.random() * pongMsgPool.length)]);
